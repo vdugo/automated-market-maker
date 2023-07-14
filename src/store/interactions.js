08b0d325fd1551d36c0e77b8fa48a316e1b9f04a
Vincent <v3rdugo@yahoo.com>
2023-07-14 12:11:29 -0700

    swaps

--- src/store/interactions.js
+++ src/store/interactions.js
@@ -14,7 +14,10 @@ import {
 
 import { 
     setContract,
-    sharesLoaded
+    sharesLoaded,
+    swapRequest,
+    swapSuccess,
+    swapFail
     } from './reducers/amm';
 
 import TOKEN_ABI from '../abis/Token.json'
@@ -70,4 +73,35 @@ export const loadBalances = async (amm, tokens, account, dispatch) => {
 
     const shares = await amm.shares(account)
     dispatch(sharesLoaded(ethers.utils.formatUnits(shares.toString(), 'ether')))
+}
+
+export const swap = async (provider, amm, token, symbol, amount, dispatch) => {
+    try {
+        // Tell Redux that the user is swapping
+        dispatch(swapRequest())
+
+        let transaction
+    
+        const signer = await provider.getSigner()
+    
+        transaction = await token.connect(signer).approve(amm.address, amount)
+        await transaction.wait()
+    
+        if (symbol === 'DAPP') {
+            transaction = await amm.connect(signer).swapToken1(amount)
+        }
+        else {
+            transaction = await amm.connect(signer).swapToken2(amount)
+        }
+    
+        await transaction.wait()
+    
+        // Tell Redux that swap has finished
+        dispatch(swapSuccess(transaction.hash))
+    }
+    catch (error) {
+        dispatch(swapFail())
+    }
+
+
 }
