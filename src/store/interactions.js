a36499733a655bc80ef162a057410856ab9ec340
Vincent <v3rdugo@yahoo.com>
2023-07-16 13:15:10 -0700

    interactions for deposits and withdrawals

--- src/store/interactions.js
+++ src/store/interactions.js
@@ -1,107 +1,176 @@
-import { ethers } from 'ethers';
+import { ethers } from 'ethers'
 
-import { 
-    setAccount,
-    setProvider,
-    setNetwork
-    } from './reducers/provider';
+import {
+  setProvider,
+  setNetwork,
+  setAccount
+} from './reducers/provider'
 
-import { 
-    setContracts,
-    setSymbols,
-    balancesLoaded
-    } from './reducers/tokens';
+import {
+  setContracts,
+  setSymbols,
+  balancesLoaded
+} from './reducers/tokens'
 
-import { 
-    setContract,
-    sharesLoaded,
-    swapRequest,
-    swapSuccess,
-    swapFail
-    } from './reducers/amm';
+import {
+  setContract,
+  sharesLoaded,
+  swapsLoaded,
+  depositRequest,
+  depositSuccess,
+  depositFail,
+  withdrawRequest,
+  withdrawSuccess,
+  withdrawFail,
+  swapRequest,
+  swapSuccess,
+  swapFail
+} from './reducers/amm'
 
-import TOKEN_ABI from '../abis/Token.json'
-import AMM_ABI from '../abis/AMM.json'
-import config from '../config.json'
+import TOKEN_ABI from '../abis/Token.json';
+import AMM_ABI from '../abis/AMM.json';
+import config from '../config.json';
 
-export const loadProvider = async (dispatch) => {
-    const provider = new ethers.providers.Web3Provider(window.ethereum)
-    dispatch(setProvider(provider))
+export const loadProvider = (dispatch) => {
+  const provider = new ethers.providers.Web3Provider(window.ethereum)
+  dispatch(setProvider(provider))
 
-    return provider
+  return provider
 }
 
 export const loadNetwork = async (provider, dispatch) => {
-    const {chainId} = await provider.getNetwork()
-    dispatch(setNetwork(chainId))
+  const { chainId } = await provider.getNetwork()
+  dispatch(setNetwork(chainId))
 
-    return chainId
+  return chainId
 }
-
 
 export const loadAccount = async (dispatch) => {
-    const accounts = await window.ethereum.request({method: 'eth_requestAccounts'})
-    const account = ethers.utils.getAddress(accounts[0])
-    dispatch(setAccount(account))
+  const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
+  const account = ethers.utils.getAddress(accounts[0])
+  dispatch(setAccount(account))
 
-    return account
+  return account
 }
 
-// Load Contracts
-
+// ------------------------------------------------------------------------------
+// LOAD CONTRACTS
 export const loadTokens = async (provider, chainId, dispatch) => {
-    const dapp = new ethers.Contract(config[chainId].dapp.address, TOKEN_ABI, provider)
-    const usd = new ethers.Contract(config[chainId].usd.address, TOKEN_ABI, provider)
+  const dapp = new ethers.Contract(config[chainId].dapp.address, TOKEN_ABI, provider)
+  const usd = new ethers.Contract(config[chainId].usd.address, TOKEN_ABI, provider)
 
-    dispatch(setContracts([dapp, usd]))
-    dispatch(setSymbols([await dapp.symbol(), await usd.symbol()]))
+  dispatch(setContracts([dapp, usd]))
+  dispatch(setSymbols([await dapp.symbol(), await usd.symbol()]))
 }
 
 export const loadAMM = async (provider, chainId, dispatch) => {
-    const amm = new ethers.Contract(config[chainId].amm.address, AMM_ABI, provider)
+  const amm = new ethers.Contract(config[chainId].amm.address, AMM_ABI, provider)
 
-    dispatch(setContract(amm))
+  dispatch(setContract(amm))
 
-    return amm
+  return amm
 }
 
+
+// ------------------------------------------------------------------------------
+// LOAD BALANCES & SHARES
 export const loadBalances = async (amm, tokens, account, dispatch) => {
-    const balance1 = ethers.utils.formatUnits((await tokens[0].balanceOf(account)).toString(), 'ether')
-    const balance2 = ethers.utils.formatUnits((await tokens[1].balanceOf(account)).toString(), 'ether')
+  const balance1 = await tokens[0].balanceOf(account)
+  const balance2 = await tokens[1].balanceOf(account)
 
-    dispatch(balancesLoaded([balance1, balance2]))
+  dispatch(balancesLoaded([
+    ethers.utils.formatUnits(balance1.toString(), 'ether'),
+    ethers.utils.formatUnits(balance2.toString(), 'ether')
+  ]))
 
-    const shares = await amm.shares(account)
-    dispatch(sharesLoaded(ethers.utils.formatUnits(shares.toString(), 'ether')))
+  const shares = await amm.shares(account)
+  dispatch(sharesLoaded(ethers.utils.formatUnits(shares.toString(), 'ether')))
 }
+
+
+// ------------------------------------------------------------------------------
+// ADD LIQUDITY
+export const addLiquidity = async (provider, amm, tokens, amounts, dispatch) => {
+  try {
+    dispatch(depositRequest())
+
+    const signer = await provider.getSigner()
+
+    let transaction
+
+    transaction = await tokens[0].connect(signer).approve(amm.address, amounts[0])
+    await transaction.wait()
+
+    transaction = await tokens[1].connect(signer).approve(amm.address, amounts[1])
+    await transaction.wait()
+
+    transaction = await amm.connect(signer).addLiquidity(amounts[0], amounts[1])
+    await transaction.wait()
+
+    dispatch(depositSuccess(transaction.hash))
+  } catch (error) {
+    dispatch(depositFail())
+  }
+}
+
+// ------------------------------------------------------------------------------
+// REMOVE LIQUDITY
+export const removeLiquidity = async (provider, amm, shares, dispatch) => {
+  try {
+    dispatch(withdrawRequest())
+
+    const signer = await provider.getSigner()
+
+    let transaction = await amm.connect(signer).removeLiquidity(shares)
+    await transaction.wait()
+
+    dispatch(withdrawSuccess(transaction.hash))
+  } catch (error) {
+    dispatch(withdrawFail())
+  }
+}
+
+// ------------------------------------------------------------------------------
+// SWAP
 
 export const swap = async (provider, amm, token, symbol, amount, dispatch) => {
-    try {
-        // Tell Redux that the user is swapping
-        dispatch(swapRequest())
+  try {
 
-        let transaction
-    
-        const signer = await provider.getSigner()
-    
-        transaction = await token.connect(signer).approve(amm.address, amount)
-        await transaction.wait()
-    
-        if (symbol === 'DAPP') {
-            transaction = await amm.connect(signer).swapToken1(amount)
-        }
-        else {
-            transaction = await amm.connect(signer).swapToken2(amount)
-        }
-    
-        await transaction.wait()
-    
-        // Tell Redux that swap has finished
-        dispatch(swapSuccess(transaction.hash))
-    }
-    catch (error) {
-        dispatch(swapFail())
+    dispatch(swapRequest())
+
+    let transaction
+
+    const signer = await provider.getSigner()
+
+    transaction = await token.connect(signer).approve(amm.address, amount)
+    await transaction.wait()
+
+    if (symbol === "DAPP") {
+      transaction = await amm.connect(signer).swapToken1(amount)
+    } else {
+      transaction = await amm.connect(signer).swapToken2(amount)
     }
 
+    await transaction.wait()
 
+    dispatch(swapSuccess(transaction.hash))
+
+  } catch (error) {
+    dispatch(swapFail())
+  }
+}
+
+
+// ------------------------------------------------------------------------------
+// LOAD ALL SWAPS
+
+export const loadAllSwaps = async (provider, amm, dispatch) => {
+  const block = await provider.getBlockNumber()
+
+  const swapStream = await amm.queryFilter('Swap', 0, block)
+  const swaps = swapStream.map(event => {
+    return { hash: event.transactionHash, args: event.args }
+  })
+
+  dispatch(swapsLoaded(swaps))
 }
